5e1768e76c6512f4da00281fb831d4eed0c8662a
Anthony Kawa <anthonyk2020@gmail.com>
2020-02-16 21:54:55 -0800

    Added Instagram text logo and left border on logo wrapper

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,12 +1,16 @@
 // You do not need to change any code in this file
 import React from "react";
 import "./SearchBar.css";
+import logo from '../../assets/instagram-logo-only-font.png';
 
 const SearchBar = (props) => {
   return (
     <div className="search-bar-wrapper">
+      <div className="logo-wrapper">
       <div className="image-wrapper">
         <i className="fab fa-instagram" />
+      </div>
+      <img className="text-logo" src={logo} />
       </div>
       <form className="search-form">
         <input
